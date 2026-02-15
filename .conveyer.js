9bbd58b62840fa2b2b4368602d37c9207b5ccba1
Eugene Nesvetaev <dev@nesvet.dev>
2026-02-15 17:26:46 +0400

    chore(common): Unify build config, update docs and LICENSE year

--- .conveyer.js
+++ .conveyer.js
@@ -1,47 +1,35 @@
 import path from "node:path";
 import { Conveyer, ESBuild } from "cnvr";
+import { common, neutral, node20 } from "../../conveyer.config.js";
 
 
 const distDir = "dist";
 
-const common = {
-	format: "esm",
-	sourcemap: true
-};
-
-
-new Conveyer([
-	
-	new ESBuild({
-		title: "Backend",
-		entryPoints: [ "src/backend/index.ts" ],
-		outfile: path.resolve(distDir, "backend/index.js"),
-		external: [ true, "insite-*" ],
-		platform: "node",
-		target: "node20",
-		...common
-	}),
-	
-	new ESBuild({
-		title: "Frontend",
-		entryPoints: [ "src/frontend/index.ts" ],
-		outfile: path.resolve(distDir, "frontend/index.js"),
-		external: true,
-		platform: "neutral",
-		target: "es2020",
-		...common
-	}),
-	
-	new ESBuild({
-		title: "Common",
-		entryPoints: [ "src/index.ts" ],
-		outfile: path.resolve(distDir, "common/index.js"),
-		external: true,
-		platform: "node",
-		target: "node20",
-		...common
-	})
-	
-], {
-	initialCleanup: distDir
-});
+new Conveyer(
+	[
+		new ESBuild({
+			title: "Backend",
+			entryPoints: [ "src/backend/index.ts" ],
+			outfile: path.resolve(distDir, "backend/index.js"),
+			...common,
+			...node20
+		}),
+		new ESBuild({
+			title: "Frontend",
+			entryPoints: [ "src/frontend/index.ts" ],
+			outfile: path.resolve(distDir, "frontend/index.js"),
+			external: true,
+			...common,
+			...neutral
+		}),
+		new ESBuild({
+			title: "Common",
+			entryPoints: [ "src/index.ts" ],
+			outfile: path.resolve(distDir, "common/index.js"),
+			external: true,
+			...common,
+			...node20
+		})
+	],
+	{ initialCleanup: distDir }
+);
