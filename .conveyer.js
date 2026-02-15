4b1690b7d502227512283ee94827bce16cff084e
Eugene Nesvetaev <dev@nesvet.dev>
2026-02-15 17:26:49 +0400

    chore(client): Unify build config, update docs and LICENSE year

--- .conveyer.js
+++ .conveyer.js
@@ -1,27 +1,20 @@
 import path from "node:path";
 import { Conveyer, ESBuild } from "cnvr";
+import { common, neutral } from "../../conveyer.config.js";
 
 
 const { NODE_ENV } = process.env;
-
 const distDir = "dist";
 
-
-new Conveyer([
-	
-	new ESBuild({
-		entryPoints: [ "src/index.ts" ],
-		outfile: path.resolve(distDir, "index.js"),
-		external: [ true, "insite-*" ],
-		platform: "neutral",
-		target: "es2020",
-		format: "esm",
-		sourcemap: true,
-		define: {
-			"process.env.NODE_ENV": JSON.stringify(NODE_ENV)
-		}
-	})
-	
-], {
-	initialCleanup: distDir
-});
+new Conveyer(
+	[
+		new ESBuild({
+			entryPoints: [ "src/index.ts" ],
+			outfile: path.resolve(distDir, "index.js"),
+			define: { "process.env.NODE_ENV": JSON.stringify(NODE_ENV) },
+			...common,
+			...neutral
+		})
+	],
+	{ initialCleanup: distDir }
+);
--- eslint.config.js
+++
@@ -1,1 +0,0 @@
-export { browser as default } from "@nesvet/eslint-config";
