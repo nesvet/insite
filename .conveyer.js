e6e4039eed5f37b045c44a46a702e4bdc5f16181
Eugene Nesvetaev <dev@nesvet.dev>
2026-02-15 17:26:56 +0400

    chore(subscriptions-server): Unify build config, update docs and LICENSE year

--- .conveyer.js
+++ .conveyer.js
@@ -1,34 +1,26 @@
 import path from "node:path";
 import { Conveyer, ESBuild } from "cnvr";
+import { common, node20 } from "../../conveyer.config.js";
 
 
 const distDir = "dist";
 
-const common = {
-	external: [ true, "insite-*" ],
-	platform: "node",
-	format: "esm",
-	sourcemap: true,
-	target: "node20"
-};
-
-
-new Conveyer([
-	
-	new ESBuild({
-		title: "index",
-		entryPoints: [ "src/index.ts" ],
-		outfile: path.resolve(distDir, "index.js"),
-		...common
-	}),
-	
-	new ESBuild({
-		title: "ws",
-		entryPoints: [ "src/ws/index.ts" ],
-		outfile: path.resolve(distDir, "ws/index.js"),
-		...common
-	})
-	
-], {
-	initialCleanup: distDir
-});
+new Conveyer(
+	[
+		new ESBuild({
+			title: "index",
+			entryPoints: [ "src/index.ts" ],
+			outfile: path.resolve(distDir, "index.js"),
+			...common,
+			...node20
+		}),
+		new ESBuild({
+			title: "ws",
+			entryPoints: [ "src/ws/index.ts" ],
+			outfile: path.resolve(distDir, "ws/index.js"),
+			...common,
+			...node20
+		})
+	],
+	{ initialCleanup: distDir }
+);
